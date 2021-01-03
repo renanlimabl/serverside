88b274ee9ac061e8ff6ea1163225e0621b9690ac
renanlimabl <renanlimabl@gmail.com>
2021-01-03 06:07:34 -0300

    Adiciona controller para appointments

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,44 +1,16 @@
 // Rotas: Receber uma requisição, chamar outro arquivo e devolver uma resposta
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentController from '../controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
-
+const appointmentController = new AppointmentController();
 /**
  * SoC: Separation of Concerns (Separação de Preocupações)
  * Cada parte do código se preocupa com algo específico.
  */
 appointmentsRouter.use(ensureAuthenticated);
 
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointments = await appointmentsRepository.find();
-
-//   return response.json(appointments);
-// });
-
-appointmentsRouter.post('/', async (request, response) => {
-  const { provider_id, date } = request.body;
-
-  const parsedDate = parseISO(date);
-
-  /**
-   * O Service espera como parâmetro = o repositório, que já foi instânciado na
-   * const = appointmentsRepository.
-   * Então basta instanciar o Service e passar o appointmentsRepository como parâmetro
-   */
-
-  // const createAppointment = new CreateAppointmentService();
-  const createAppointment = container.resolve(CreateAppointmentService);
-
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id,
-  });
-
-  return response.json(appointment);
-});
+appointmentsRouter.post('/', appointmentController.create);
 
 export default appointmentsRouter;
