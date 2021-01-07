bb94dc13d194cc4a7193fdd89e2650ed3a07178e
renanlimabl <renanlimabl@gmail.com>
2021-01-07 09:22:30 -0300

    Adiciona teste para nao autenticar se a senha for errada

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -33,7 +33,7 @@ describe('AuthenticateUser', () => {
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
   });
-  it('should be not able to authenticate with non existing', async () => {
+  it('should not be able to authenticate with non existing', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
     const fakeHashProvider = new FakeHashProvider();
 
@@ -46,6 +46,33 @@ describe('AuthenticateUser', () => {
       authenticateUser.execute({
         email: 'johndoe@example.com',
         password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: 'wrong-pass',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
